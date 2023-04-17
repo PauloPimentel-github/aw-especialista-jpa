e954e7afb2685673034f0fad63b9b921fcda4e63
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2023-04-17 19:24:41 -0300

    First commit - Adicionando estrutura inicial do projeto

---
+++ src/main/java/com/phimentel/awespecialistajpa/AwEspecialistaJpaApplication.java
@@ -0,0 +1,13 @@
+package com.phimentel.awespecialistajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwEspecialistaJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwEspecialistaJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phimentel/awespecialistajpa/AwEspecialistaJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phimentel.awespecialistajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwEspecialistaJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
