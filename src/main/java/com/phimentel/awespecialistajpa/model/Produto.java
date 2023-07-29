fece8cfb625eaf141e88e9b64c946201f7c559ef
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2023-07-29 14:36:29 -0300

    Mapeamento avançado -> 6.13. Exercício: persistindo fotos de produtos

--- src/main/java/com/phimentel/awespecialistajpa/model/Produto.java
+++ src/main/java/com/phimentel/awespecialistajpa/model/Produto.java
@@ -26,6 +26,9 @@ public class Produto {
 
     private BigDecimal preco;
 
+    @Lob
+    private byte[] foto;
+
     @Column(name = "data_criacao", updatable = false)
     private LocalDateTime dataCriacao;
 
