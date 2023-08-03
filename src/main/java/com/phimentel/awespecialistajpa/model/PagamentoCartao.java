b0245a6735a49d983709550b8039dada77747417
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2023-08-03 12:17:08 -0300

    Mapeamento avançado -> 6.17. Mapeando herança com estratégia de tabela única (single table)

--- src/main/java/com/phimentel/awespecialistajpa/model/PagamentoCartao.java
+++ src/main/java/com/phimentel/awespecialistajpa/model/PagamentoCartao.java
@@ -3,12 +3,14 @@ package com.phimentel.awespecialistajpa.model;
 import lombok.Data;
 
 import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Data
+@DiscriminatorValue("cartao")
 @Entity
-@Table(name = "pagamentos_cartao")
+//@Table(name = "pagamentos_cartao")
 public class PagamentoCartao extends Pagamento {
 
     @Column(name = "numero_cartao")
